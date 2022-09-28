baa05316158c1d32da58673951d18a1970fa9d71
Soph H.P <smkendall90@gmail.com>
2022-09-28 15:25:01 +0200

    responsiveness of moderation page

--- src/components/layout/SightingsGrid/index.styled.tsx
+++ src/components/layout/SightingsGrid/index.styled.tsx
@@ -1,19 +1,35 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const StyledGridContainer = styled.div<{ $moderation?: boolean }>`
   display: grid;
   height: fit-content;
   margin: 0 auto;
-  grid-template-columns: repeat(auto-fit, minmax(max(260px, 200px), ${({ $moderation }) => $moderation ? '300px' : '1fr'}));
+  grid-template-columns: repeat(auto-fit, minmax(max(260px, 200px), 1fr));
   gap: 10px;
 
   h1 {
     width: 100%;
   }
+
+  ${({ $moderation }) =>
+    $moderation &&
+    css`
+      @media (min-width: 1000px) {
+        grid-template-columns: repeat(auto-fit, minmax(max(260px, 200px), 300px));
+      }
+    `}
 `;
 
 export const SightingsContainer = styled.div<{ $moderation?: boolean }>`
   display: flex;
   flex-direction: column;
-  width: ${({ $moderation }) => ($moderation ? '100%' : '60%')};
+  width: 60%;
+
+  ${({ $moderation }) =>
+    $moderation &&
+    css`
+      width: 100%;
+      max-width: 1200px;
+      margin: 0 auto;
+    `}
 `;
