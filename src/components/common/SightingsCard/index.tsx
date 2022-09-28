6eaffaa8c85d4103961819b94e17164f495ce05d
Soph H.P <smkendall90@gmail.com>
2022-09-28 11:56:53 +0200

    solve a nested anchors error

--- src/components/common/SightingsCard/index.tsx
+++ src/components/common/SightingsCard/index.tsx
@@ -2,32 +2,35 @@ import findTimeAgo from '../../../utils/findTimeAgo';
 import { SightingInterface } from '../../../hooks/useSightings';
 import { ImageWrapper, InfoWrapper, StyledCardContainer } from './index.styled';
 import VarifiedUsername from '../VarifiedUsername';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const SightingsCard = ({ sighting }: { sighting: SightingInterface }) => {
   const noImage = !sighting.attributes.photos.data;
   const imageSrc = noImage ? '' : sighting.attributes.photos.data[0].attributes.url;
   const { date: when, nearestLocation, species, userId, username, varified } = sighting.attributes;
 
+  const navigate = useNavigate();
+
   return (
-    <StyledCardContainer $isVarified={varified}>
-      <Link to={`/sighting/${sighting.id}`}>
-        <ImageWrapper $height={160} $noImage={noImage}>
-          <img src={imageSrc} alt={species} />
-        </ImageWrapper>
-        <h2>{species}</h2>
-        <InfoWrapper>
-          <span>When: </span>
-          {findTimeAgo(when)}
-        </InfoWrapper>
-        <InfoWrapper>
-          <span>Where: </span>
-          {nearestLocation}
-        </InfoWrapper>
-        <InfoWrapper>
-          <VarifiedUsername userId={userId} backupUsername={username} />
-        </InfoWrapper>
-      </Link>
+    <StyledCardContainer
+      $isVarified={varified}
+      onClick={() => navigate(`/sighting/${sighting.id}`)}
+    >
+      <ImageWrapper $height={160} $noImage={noImage}>
+        <img src={imageSrc} alt={species} />
+      </ImageWrapper>
+      <h2>{species}</h2>
+      <InfoWrapper>
+        <span>When: </span>
+        {findTimeAgo(when)}
+      </InfoWrapper>
+      <InfoWrapper>
+        <span>Where: </span>
+        {nearestLocation}
+      </InfoWrapper>
+      <InfoWrapper>
+        <VarifiedUsername userId={userId} backupUsername={username} />
+      </InfoWrapper>
     </StyledCardContainer>
   );
 };
